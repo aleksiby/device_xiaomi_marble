31ea4bf3f28a04c9c85af2321830abbec001840f
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2025-11-23 19:15:55 +0700

    fixup! marble: Include Aospa Dolby Atmos * Using dolby from: https://github.com/xiaomi-sm8450-marble/android_hardware_dolby [1]: sm8450-common: Add dolby volume listener effect [2]: sm8450-common: audio: Bring all stream effects [3]: sm8450-common: HIDL: Move all hidl files to devicetree [4]: sm8450-common: audio: Sync with Spatial Audio
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
    Signed-off-by: Hafidz Muzakky <nekoskuy.ubuntu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,7 +207,9 @@ DEVICE_MANIFEST_SKUS := ukee
 DEVICE_MANIFEST_UKEE_FILES := \
     $(DEVICE_PATH)/vintf/manifest_ukee.xml \
     $(DEVICE_PATH)/vintf/manifest_marble.xml \
-    $(DEVICE_PATH)/vintf/manifest_no_nfc.xml
+    $(DEVICE_PATH)/vintf/manifest_no_nfc.xml \
+    $(DEVICE_PATH)/vintf/vendor.dolby.media.c2.xml \
+    $(DEVICE_PATH)/vintf/vendor.dolby.hardware.dms@2.0-service.xml
 
 ODM_MANIFEST_SKUS += marble
 ODM_MANIFEST_MARBLE_FILES += $(DEVICE_PATH)/vintf/manifest_nfc.xml
@@ -215,6 +217,7 @@ ODM_MANIFEST_MARBLE_FILES += $(DEVICE_PATH)/vintf/manifest_nfc.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/vintf/dolby_framework_matrix.xml \
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/vintf/framework_manifest.xml
 
